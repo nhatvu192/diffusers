d459484781949e7e7ecd1b5b1e0078a0444e1593
Nhat Vu <53397242+nhatvu192@users.noreply.github.com>
2024-01-31 06:57:41 +0000

    Process white_background

--- process_data.py
+++ process_data.py
@@ -2,6 +2,9 @@ import argparse
 import xml.etree.ElementTree as ET
 import cairosvg
 import os
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
 
 
 def kanjivg_to_svg(root):
@@ -23,6 +26,11 @@ def svg_to_png(svg, output_file: str) -> None:
 
 def hex_to_kanji(hex_number: str) -> str:
     return chr(int(hex_number, 16))
+
+def transparent_to_white_background(image: np.ndarray) -> np.ndarray:
+    image = np.where(image[..., -1] == 0, 255, 0).astype(np.uint8)
+    image = np.repeat(image.reshape(*(image.shape), 1), 3, axis=2)
+    return image
 
 def get_kanji_to_meanings(kanjidic2_dir: str) -> dict:
 
@@ -80,10 +88,15 @@ if __name__ == "__main__":
     os.makedirs(os.path.join(args.output_dir, "white_background"), exist_ok=True)
 
     with open(os.path.join(args.output_dir, "metadata.jsonl"), "w") as output_metadata:
-        for root in kanjivg_root:
+        for root in tqdm(kanjivg_root):
             kanji_character = hex_to_kanji(root.attrib["id"].split("_")[-1])
             if kanji_character in kanji_to_meanings:
                 transparent_path = os.path.join(args.output_dir, "transparent", f"{kanji_character}.png")
                 svg_to_png(kanjivg_to_svg(root), transparent_path)
+
+                with Image.open(transparent_path) as image:
+                    image = transparent_to_white_background(np.array(image))
+                    image = Image.fromarray(image)
+                    image.save(os.path.join(args.output_dir, "white_background", f"{kanji_character}.png"))
 
 
